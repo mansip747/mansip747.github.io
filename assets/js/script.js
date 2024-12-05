68e2d2fdb104a917cc051e856fd3c30dfa9845ed
Mansi Patel <mansip747@gmail.com>
2024-12-05 11:20:05 -0800

    mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Product Management", "Data Analysis", "Business Analysis", "Marketing Analysis", "Product Growth Analyst","Machine Learning"],
+    strings: ["Product Management", "Data Analysis", "Business Analysis", "Marketing Analysis", "Product Growth Analysis","Machine Learning"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
